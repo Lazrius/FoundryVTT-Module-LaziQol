53192c13a3129b20bdcb8e6105b1a8a60d7e679e
Lazrius <lazdisco@gmail.com>
2022-03-24 12:47:28 +0000

    feat: add calculation for prepared spell count

--- Source/Hooks/RenderActorSheet.ts
+++ Source/Hooks/RenderActorSheet.ts
@@ -6,7 +6,6 @@ export const RenderActorSheet = async (app: ActorSheet5e, html: JQuery): Promise
 		return;
 	}
 
-
 	const maxPreparedSpells = (app.actor.data.data.details as any).maxPreparedSpells as string | undefined;
 	if (maxPreparedSpells === undefined) {
 		return;
@@ -16,6 +15,34 @@ export const RenderActorSheet = async (app: ActorSheet5e, html: JQuery): Promise
 	// @ts-ignore
 	await ApplyActiveEffect(app.actor, { key: "data.details.maxPreparedSpells", value: maxPreparedSpells });
 
-	console.log(app);
-	console.log(html);
+	const spellBookFilter = html.find('ul.filter-list[data-filter="spellbook"]:first');
+	if (!spellBookFilter)
+		return;
+
+	const preparedSpellElement = spellBookFilter.find('li[data-filter="prepared"]:first');
+	if (!preparedSpellElement)
+		return;
+
+	const OnPreparedCountChange = (evt: Event): void => {
+		if (!evt.currentTarget)
+			return;
+
+		const target = $(evt.currentTarget);
+		if (!target)
+			return;
+
+		const html = target.html();
+		const match = target.html().match(/(\d+)/g);
+		if (!match)
+			return;
+
+		const outOf = match[0] + '/' + (app.actor.data.data.details as any).maxPreparedSpells;
+
+		preparedSpellElement.off('change');
+		target.html(html.replace(match[0], outOf));
+		preparedSpellElement.on('change', OnPreparedCountChange);
+	}
+
+	preparedSpellElement.on('change', OnPreparedCountChange);
+	preparedSpellElement.trigger('change');
 };
