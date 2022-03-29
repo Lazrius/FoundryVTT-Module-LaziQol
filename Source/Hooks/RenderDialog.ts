b8b0f1addf65b0f18dd4843cd3dc1447c03cfa09
Lazrius <lazdisco@gmail.com>
2022-03-29 11:37:31 +0100

    fix: adjust render dialog to not show prepared spell reset for non-prepared casters (for #2)

--- Source/Hooks/RenderDialog.ts
+++ Source/Hooks/RenderDialog.ts
@@ -2,6 +2,10 @@ import {PlayerRest} from "./PlayerRest";
 
 export const RenderDialog = (app: Dialog, html: JQuery): void => {
 	if (app.options.template.endsWith('long-rest.html')) {
+		const actor = (app as any).actor as Actor5e | undefined;
+		if (!actor || !((actor.data.data.details as any).maxPreparedSpells))
+			return;
+
 		PlayerRest.OnPlayerRest(app, html);
 		return;
 	}
