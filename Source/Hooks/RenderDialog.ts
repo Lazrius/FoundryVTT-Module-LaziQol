b5fa2b8818d205de2d795461bb17d1d5e07e4e69
Lazrius <lazdisco@gmail.com>
2022-03-29 12:10:31 +0100

    style/fix: remove long-rest dialog height specifier so it doesn't visually break

--- Source/Hooks/RenderDialog.ts
+++ Source/Hooks/RenderDialog.ts
@@ -6,6 +6,8 @@ export const RenderDialog = (app: Dialog, html: JQuery): void => {
 		if (!actor || !((actor.data.data.details as any).maxPreparedSpells))
 			return;
 
+		// Unset height to allow for our element to be properly rendered
+		html.css('height', '');
 		PlayerRest.OnPlayerRest(app, html);
 		return;
 	}
